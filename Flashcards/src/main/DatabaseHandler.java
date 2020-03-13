32131d17293fb88d674db3cc4765d232ebe462b4
Ivan Baradi <ivan4ubc@yahoo.com>
2020-03-13 13:51:08 -0700

    Committed

--- Flashcards/src/main/DatabaseHandler.java
+++ Flashcards/src/main/DatabaseHandler.java
@@ -1,6 +1,6 @@
 package main;
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
+import java.util.ArrayList;
 
 /***
  * DatabaseHandler is responsible for adding flashcards
@@ -21,7 +21,7 @@ public class DatabaseHandler {
 			try {
 				String driver = "com.mysql.cj.jdbc.Driver";
 				String url = "jdbc:mysql://localhost:3306/Flashcards";
-				String username = "root", password = "pass";
+				String username = "root", password = "kidneyfella361";
 				Class.forName(driver);
 				Connection connection = DriverManager.getConnection(url, username, password);
 				System.out.println("Database Connected!");
@@ -33,16 +33,85 @@ public class DatabaseHandler {
 			return null;
 		}
 	
+	/**
+	 * Creates a flashcard table
+	 * @param newSet
+	 */
 	public static void addFlashCardSet(String newSet) {
 		
 		try {
 			
 			Connection connect = getConnection();
-			
+			PreparedStatement add = connect.prepareStatement("CREATE TABLE " + newSet + "(flashcard_id INT AUTO_INCREMENT PRIMARY KEY, question TEXT, answer TEXT);");
+			add.executeUpdate();
+			System.out.println("Flashcard Set Added!!!");
 		}catch(Exception e) {
-			
+			System.out.println(e);
 		}
 		
 	}
-
+	
+	/**
+	 * Adds a flashcard to a specified flashcard set
+	 * @param question
+	 * @param answer
+	 * @param flashcardSet
+	 */
+	public static void addFlashCard(String question, String answer, String flashcardSet) {
+		
+		try {
+			Connection connect = getConnection();
+			PreparedStatement add = connect.prepareStatement("INSERT INTO " + flashcardSet + "VALUES(" + question + "," + answer + ");");
+			add.executeUpdate();
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+		
+	}
+	
+	/**
+	 * Returns all flashcard sets from the flashcard database
+	 * @return 
+	 */
+	public static void getAllFlashCardSets(){
+		
+		ArrayList<String> flashcardSets = new ArrayList<>();
+		
+		try {
+			Connection connect = getConnection();
+			PreparedStatement getAll = connect.prepareStatement("SELECT TABLE_NAME FROM information_schema.tables WHERE table_schema = 'Flashcards';");
+			ResultSet res = getAll.executeQuery();
+			
+			while(res.next()) {
+				System.out.println(res.getString("TABLE_NAME"));
+			}
+			
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+		
+		//return flashcardSets;
+	}
+	
+	/**
+	 * Checks if the flashcard set exists
+	 * @param set
+	 * @return
+	 */
+	public static boolean flashcardSetExists(String set) {
+		
+		try {
+			
+			Connection connect = getConnection();
+			PreparedStatement get = connect.prepareStatement("SELECT * FROM flashcard_sets WHERE flashcard_setName = '" + set + "'");
+			ResultSet res = get.executeQuery();
+			
+			if(res.next())
+				return true;
+		}catch(Exception e) {
+			System.out.print(e);
+		}
+		
+		return false;
+	}
 }
--- Flashcards/src/main/Main.java
+++ Flashcards/src/main/Main.java
@@ -19,6 +19,7 @@ interface Menus {
 	JPanel viewFlashcards();
 	JPanel deleteFlashcard();
 	JPanel takeQuiz();
+	JPanel message(int message_id);
 }
 
 public class Main extends JFrame implements Menus{
@@ -30,7 +31,8 @@ public class Main extends JFrame implements Menus{
 	 */
 	public Main() {
 		
-		add(mainMenu());
+		//add(mainMenu());
+		add(message(0));
 		
 		setTitle("Flashcards");
 		setSize(400,400);
@@ -40,8 +42,8 @@ public class Main extends JFrame implements Menus{
 
 	//Driver function
 	public static void main(String[] args) throws Exception {
-
-		new Main();
+		//new Main();
+		DatabaseHandler.getConnection();
 	}
 	
 	/**
@@ -159,7 +161,88 @@ public class Main extends JFrame implements Menus{
 
 	@Override
 	public JPanel createFlashcardSet() {
-		return null;	
+		
+		JPanel main = new JPanel();
+		main.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		
+		JLabel title = new JLabel("Create a Flashcard Set");
+		title.setFont(new Font("Helvetica", Font.BOLD, 16));
+		c.gridx = 0;
+		c.gridy = 0;
+		main.add(title,c);
+		
+		JLabel instruction = new JLabel("Enter the name of the flashcard set");
+		instruction.setFont(new Font("Helvetica", Font.PLAIN, 12));
+		c.gridx = 0;
+		c.gridy = 1;
+		c.insets = new Insets(10,0,0,0);
+		main.add(instruction,c);
+		
+		JLabel err_message = new JLabel("Flashcard set already exists!");
+		err_message.setForeground(Color.RED);
+		err_message.setFont(new Font("Helvetica", Font.PLAIN, 11));
+		err_message.setVisible(false); //Set false later
+		c.gridx = 0;
+		c.gridy = 2;
+		c.insets = new Insets(10,4,0,0);
+		c.anchor = GridBagConstraints.WEST;
+		main.add(err_message,c);
+				
+		JTextField flashcardName = new JTextField();
+		flashcardName.setPreferredSize(new Dimension(200,30));
+		c.gridx = 0;
+		c.gridy = 3;
+		c.anchor = GridBagConstraints.CENTER;
+		c.insets = new Insets(0,0,0,0);
+		main.add(flashcardName,c);
+		
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(new GridBagLayout());
+		
+		JButton back = new JButton("Back");
+		back.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				switchPanel(main, mainMenu());
+			}
+		});
+		c.gridx = 0;
+		c.gridy = 0;
+		c.insets = new Insets(5,0,0,0);
+		buttonPanel.add(back,c);
+		
+		JButton submit = new JButton("Submit");
+		submit.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				try {
+					
+					if(DatabaseHandler.flashcardSetExists(flashcardName.getText())) {
+						err_message.setVisible(true);
+					}else {
+						DatabaseHandler.addFlashCardSet(flashcardName.getText());
+						switchPanel(main, message(0));
+					}
+				}catch(Exception ex) {
+					System.out.println(ex);
+				}
+			}
+			
+		});
+		c.gridx = 1;
+		c.gridy = 0;
+		c.insets = new Insets(5,10,0,0);
+		buttonPanel.add(submit,c);
+		
+		//Adds button panel to main
+		c.gridx = 0;
+		c.gridy = 4;
+		main.add(buttonPanel,c);
+		
+		return main;
 	}
 
 	@Override
@@ -180,5 +263,52 @@ public class Main extends JFrame implements Menus{
 	@Override
 	public JPanel takeQuiz() {
 		return null;
+	}
+
+	/**
+	 * Displays a message before the user can go back.
+	 * parameters: message_id (indicates unique message) 
+	 * 
+	 * 0: Flashcard Set is successfully added
+	 */
+	@Override
+	public JPanel message(int message_id) {
+		
+		JPanel main = new JPanel();
+		main.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		JLabel title;
+		
+		//Selects a message to display 
+		if(message_id == 0) {
+			title = new JLabel("Success!");
+			title.setFont(new Font("Helvetica", Font.BOLD, 25));
+			title.setForeground(new Color(34,112,48));
+			c.gridx = 0;
+			c.gridy = 0;
+			main.add(title,c);
+			
+			JLabel suc_message = new JLabel("Your flashcard set is successfully added!");
+			c.gridx = 0;
+			c.gridy = 1;
+			c.insets = new Insets(5,0,0,0);
+			main.add(suc_message,c);
+		}
+		
+		//Back Button
+		JButton back = new JButton("Main Menu");
+		back.setPreferredSize(new Dimension(150,30));
+		back.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				switchPanel(main, mainMenu());
+			}
+		});
+		c.gridx = 0;
+		c.gridy = 2;
+		c.insets = new Insets(20,0,0,0);
+		main.add(back,c);
+		
+		return main;
 	}
 }
